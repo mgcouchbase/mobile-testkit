73b95029952f8bc1d0c7d300768be147067050c9
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-05-17 11:47:24 -0700

    Saving changes

--- testsuites/lite/conftest.py
+++ testsuites/lite/conftest.py
@@ -10,18 +10,18 @@ from keywords.MobileRestClient import MobileRestClient
 
 
 def pytest_addoption(parser):
-    parser.addoption("--client-one-platform", action="store", help="client-one-platform: the platform to assign to the first client")
-    parser.addoption("--client-one-version", action="store", help="client-one-version: the version to download / install for the first client")
-    parser.addoption("--client-one-host", action="store", help="client-one-host: the host to start to the first client on")
-    parser.addoption("--client-one-port", action="store", help="client-one-port: the port to assign to the first client")
-    parser.addoption("--client-one-mode", action="store", help="client-one-mode: the mode of sync_gateway to run tests against, channel_cache ('cc') or distributed_index ('di')")
-    parser.addoption("--client-one-storage-engine", action="store", help="client-one-storage-engine: the storage engine to use with the first client")
+    parser.addoption("--server-platform", action="store", help="server-platform: the platform to assign to the first client")
+    parser.addoption("--server-version", action="store", help="server-version: the version to download / install for the first client")
+    parser.addoption("--server-host", action="store", help="server-host: the host to start to the first client on")
+    parser.addoption("--server-port", action="store", help="server-port: the port to assign to the first client")
+    parser.addoption("--server-mode", action="store", help="server-mode: the mode of sync_gateway to run tests against, channel_cache ('cc') or distributed_index ('di')")
+    parser.addoption("--server-storage-engine", action="store", help="server-storage-engine: the storage engine to use with the first client")
 
-    parser.addoption("--client-two-platform", action="store", help="client-two-platform: the platform to assign to the first client")
-    parser.addoption("--client-two-version", action="store", help="client-two-version: the version to download / install for the first client")
-    parser.addoption("--client-two-host", action="store", help="client-two-host: the host to start to the first client on")
-    parser.addoption("--client-two-port", action="store", help="client-two-port: the port to assign to the first client")
-    parser.addoption("--client-two-storage-engine", action="store", help="client-two-storage-engine: the storage engine to use with the first client")
+    parser.addoption("--client-platform", action="store", help="client-platform: the platform to assign to the first client")
+    parser.addoption("--client-version", action="store", help="client-version: the version to download / install for the first client")
+    parser.addoption("--client-host", action="store", help="client-host: the host to start to the first client on")
+    parser.addoption("--client-port", action="store", help="client-port: the port to assign to the first client")
+    parser.addoption("--client-storage-engine", action="store", help="client-storage-engine: the storage engine to use with the first client")
     parser.addoption("--cbs-server-version", action="store", help="cbs-server-version: version of Couchbase Server to install and run tests against")
     parser.addoption("--skip-provisioning", action="store_true", help="Skip cluster provisioning at setup", default=False)
 
@@ -33,31 +33,31 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="session")
 def setup_client_suite(request):
     """Suite setup fixture for client tests"""
-    client_one_platform = request.config.getoption("--client-one-platform")
-    client_one_version = request.config.getoption("--client-one-version")
-    client_one_host = request.config.getoption("--client-one-host")
-    client_one_port = request.config.getoption("--client-one-port")
-    client_one_storage_engine = request.config.getoption("--client-one-storage-engine")
+    server_platform = request.config.getoption("--server-platform")
+    server_version = request.config.getoption("--server-version")
+    server_host = request.config.getoption("--server-host")
+    server_port = request.config.getoption("--server-port")
+    server_storage_engine = request.config.getoption("--server-storage-engine")
 
-    client_two_platform = request.config.getoption("--client-two-platform")
-    client_two_version = request.config.getoption("--client-two-version")
-    client_two_host = request.config.getoption("--client-two-host")
-    client_two_port = request.config.getoption("--client-two-port")
-    client_two_storage_engine = request.config.getoption("--client-two-storage-engine")
+    client_platform = request.config.getoption("--client-platform")
+    client_version = request.config.getoption("--client-version")
+    client_host = request.config.getoption("--client-host")
+    client_port = request.config.getoption("--client-port")
+    client_storage_engine = request.config.getoption("--client-storage-engine")
 
-    if client_one_platform == "sync_gateway" and client_two_platform == "sync_gateway":
+    if server_platform == "sync_gateway" and client_platform == "sync_gateway":
         raise ProvisioningError("Both platforms cannot be sync_gateway")
 
-    # We'll enforce client_one as SG for SG <-> CBL replication
-    if client_two_platform == "sync_gateway":
-        raise ProvisioningError("sync_gateway has to be client-one-platform")
+    # We'll enforce server as SG for SG <-> CBL replication
+    if client_platform == "sync_gateway":
+        raise ProvisioningError("sync_gateway has to be server-platform")
 
-    if client_one_platform == "sync_gateway":
+    if server_platform == "sync_gateway":
         # SG <-> CBL replication
-        client_one = "sync_gateway"
+        server = "sync_gateway"
         skip_provisioning = request.config.getoption("--skip-provisioning")
-        sync_gateway_version = client_one_version
-        sync_gateway_mode = request.config.getoption("--client-one-mode")
+        sync_gateway_version = server_version
+        sync_gateway_mode = request.config.getoption("--server-mode")
         cbs_server_version = request.config.getoption("--cbs-server-version")
 
         cluster_config = "{}/base_{}".format(CLUSTER_CONFIGS_DIR, sync_gateway_mode)
@@ -75,40 +75,41 @@ def setup_client_suite(request):
     else:
         # No SG, its P2P replication
         # We'll setup client 1 here
-        client_one = LiteServFactory.create(platform=client_one_platform,
-                                            version_build=client_one_version,
-                                            host=client_one_host,
-                                            port=client_one_port,
-                                            storage_engine=client_one_storage_engine)
+        server = LiteServFactory.create(platform=server_platform,
+                                        version_build=server_version,
+                                        host=server_host,
+                                        port=server_port,
+                                        storage_engine=server_storage_engine)
 
         log_info("Downloading LiteCoreServ ...")
         # Download LiteServ
-        client_one.download()
+        server.download()
 
         # Install LiteServ
-        client_one.install()
+        server.install()
 
     # Client 2 setup is common
-    client_two = LiteServFactory.create(platform=client_two_platform,
-                                        version_build=client_two_version,
-                                        host=client_two_host,
-                                        port=client_two_port,
-                                        storage_engine=client_two_storage_engine)
+    client = LiteServFactory.create(platform=client_platform,
+                                    version_build=client_version,
+                                    host=client_host,
+                                    port=client_port,
+                                    storage_engine=client_storage_engine)
 
     log_info("Downloading LiteCoreServ ...")
     # Download LiteServ
-    # client_two.download()
+    # client.download()
 
     # Install LiteServ
-    # client_two.install()
+    # client.install()
 
     # Wait at the yeild until tests referencing this suite setup have run,
     # Then execute the teardown
     yield {
-        "client_one": client_one,
+        "server": server,
+        "server_platform": server_platform,
         "cluster_config": cluster_config,
         "sg_mode": sync_gateway_mode,
-        "client_two": client_two
+        "client": client
     }
 
     log_info("Tearing down suite ...")
@@ -120,8 +121,9 @@ def setup_client_test(request, setup_client_suite):
     """Test setup fixture for client tests"""
     log_info("Setting up client test ...")
 
-    client_one = setup_client_suite["client_one"]
-    client_two = setup_client_suite["client_two"]
+    server = setup_client_suite["server"]
+    server_platform = setup_client_suite["server_platform"]
+    client = setup_client_suite["client"]
     cluster_config = setup_client_suite["cluster_config"]
     test_name = request.node.name
 
@@ -141,10 +143,10 @@ def setup_client_test(request, setup_client_suite):
     # Yield values to test case via fixture argument
     yield {
         "cluster_config": cluster_config,
-        "sg_mode": setup_client_suite["sg_mode"],
         "ls_url_two": ls_url_two,
         "ls_url_one": sg_url,
-        "ls_url_one_admin": sg_admin_url
+        "ls_url_one_admin": sg_admin_url,
+        "server_platform": server_platform
     }
 
     log_info("Tearing down test")
--- testsuites/lite/test_replication.py
+++ testsuites/lite/test_replication.py
@@ -1,7 +1,14 @@
 import pytest
+import time
+
+from requests import Session
 
 from keywords.utils import log_info
 from keywords.MobileRestClient import MobileRestClient
+from keywords.document import create_docs
+from keywords.verification import verify_docs_present
+from keywords.exceptions import TimeoutException
+from keywords.constants import CLIENT_REQUEST_TIMEOUT
 
 
 @pytest.mark.sanity
@@ -9,49 +16,61 @@ from keywords.MobileRestClient import MobileRestClient
 @pytest.mark.syncgateway
 @pytest.mark.replication
 @pytest.mark.session
-def test_replication(setup_client_test):
+@pytest.mark.parametrize("num_docs_per_db, seeded_client_db, replication_type, continuous", [
+    (100, False, "push", True),
+])
+def test_replication(setup_client_test, num_docs_per_db, seeded_client_db, replication_type, continuous):
     ls_url_one = setup_client_test["ls_url_one"]
     ls_url_two = setup_client_test["ls_url_two"]
-    sg_mode = setup_client_test["sg_mode"]
+    server_platform = setup_client_test["server_platform"]
 
     num_docs_per_db = 100
     client = MobileRestClient()
 
     log_info("Creating databases")
 
-    if sg_mode:
+    if server_platform == "sync_gateway":
         ls_db1 = "db"
+        blip_url_one = ls_url_one.replace("http", "blip")
     else:
         ls_db1 = client.create_database(url=ls_url_one, name="ls_db1")
-        blip_url_one = ls_url_one.replace("http", "blip")
 
     ls_db2 = client.create_database(url=ls_url_two, name="ls_db2")
-    blip_url_one = ls_url_one.replace("http", "blip")
-    blip_url_two = ls_url_two.replace("http", "blip")
 
     log_info("ls_url_one: {}".format(ls_url_one))
     log_info("ls_url_two: {}".format(ls_url_two))
 
-    # Setup continuous push / pull replication from client 2 ls_db2 to client 1 ls_db1
-    repl_three = client.start_replication(
+    if seeded_client_db:
+        bulk_docs = create_docs("test_ls_db2_seed", num_docs_per_db)
+        ls_db2_docs_seed = client.add_bulk_docs(url=ls_url_two, db=ls_db2, docs=bulk_docs)
+        assert len(ls_db2_docs_seed) == num_docs_per_db
+
+    from_db = None
+    from_url = None
+    to_db = None
+    to_url = None
+
+    if replication_type == "push":
+        from_db = ls_db2
+        to_url = blip_url_one
+        to_db = ls_db1
+    elif replication_type == "pull":
+        from_db = ls_db1
+        from_url = blip_url_one
+        to_db = ls_db2
+
+    # Setup replication from from_db to to_db
+    repl_one = client.start_replication(
         url=ls_url_two,
-        continuous=True,
-        from_db=ls_db2,
-        to_url=blip_url_one, to_db=ls_db1
+        continuous=continuous,
+        from_db=from_db, from_url=from_url,
+        to_url=to_url, to_db=to_db
     )
 
-    repl_four = client.start_replication(
-        url=ls_url_two,
-        continuous=True,
-        from_url=blip_url_one, from_db=ls_db1,
-        to_db=ls_db2
-    )
-
-    client.wait_for_replication_status_idle(url=ls_url_two, replication_id=repl_three)
-    client.wait_for_replication_status_idle(url=ls_url_two, replication_id=repl_four)
+    client.wait_for_replication_status_idle(url=ls_url_two, replication_id=repl_one)
 
     ls_url_two_replications = client.get_replications(ls_url_two)
-    assert len(ls_url_two_replications) == 2
+    assert len(ls_url_two_replications) == 1
 
     ls_db1_docs = client.add_docs(url=ls_url_one, db=ls_db1, number=num_docs_per_db, id_prefix="test_ls_db1")
     assert len(ls_db1_docs) == num_docs_per_db
@@ -59,11 +78,37 @@ def test_replication(setup_client_test):
     ls_db2_docs = client.add_docs(url=ls_url_two, db=ls_db2, number=num_docs_per_db, id_prefix="test_ls_db2")
     assert len(ls_db2_docs) == num_docs_per_db
 
-    all_docs = client.merge(ls_db1_docs, ls_db2_docs)
-    assert len(all_docs) == 200
+    # all_docs = client.merge(ls_db1_docs, ls_db2_docs)
+    all_docs = ls_db2_docs
+    assert len(all_docs) == 100
 
-    client.verify_docs_present(url=ls_url_one, db=ls_db1, expected_docs=all_docs)
-    client.verify_docs_present(url=ls_url_two, db=ls_db2, expected_docs=all_docs)
+    # client.verify_docs_present(url=ls_url_one, db=ls_db1, expected_docs=all_docs)
+    sg_expected_doc_map = {all_doc["id"]: all_doc["rev"] for all_doc in all_docs}
 
-    client.verify_docs_in_changes(url=ls_url_one, db=ls_db1, expected_docs=all_docs)
-    client.verify_docs_in_changes(url=ls_url_two, db=ls_db2, expected_docs=all_docs)
+    session = Session()
+    resp = session.get("{}/{}/_all_docs".format(ls_url_one, ls_db1))
+    resp.raise_for_status()
+    resp_obj = resp.json()
+
+    start = time.time()
+    while True:
+        if time.time() - start > CLIENT_REQUEST_TIMEOUT:
+            raise TimeoutException("Verify Docs Present: TIMEOUT")
+
+        docs_present = verify_docs_present(sg_expected_doc_map, resp_obj, "sync_gateway")
+        if docs_present:
+            break
+        else:
+            log_info("Retrying to verify all docs are present...")
+
+    # client.verify_docs_present(url=ls_url_two, db=ls_db2, expected_docs=all_docs)
+
+    # client.verify_docs_in_changes(url=ls_url_one, db=ls_db1, expected_docs=all_docs)
+    # client.verify_docs_in_changes(url=ls_url_two, db=ls_db2, expected_docs=all_docs)
+
+    client.stop_replication(
+        url=ls_url_two,
+        continuous=continuous,
+        from_db=from_db, from_url=from_url,
+        to_url=to_url, to_db=to_db
+    )
