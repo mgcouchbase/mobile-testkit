bda58f935327b65711d7e3a3c93a4ebf26348733
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-05-18 20:39:37 -0700

    save work

--- keywords/verification.py
+++ keywords/verification.py
@@ -3,17 +3,37 @@ import logging
 from keywords.utils import log_info
 
 
-def verify_docs_present(expected_docs, actual_docs, server_type):
+def verify_docs_present_listener(expected_docs, actual_docs):
     missing_docs = []
 
     for actual_doc in actual_docs["rows"]:
         if "error" in actual_doc or ("value" in actual_doc and len(actual_doc["value"]) == 0):
             # Doc not found
             missing_docs.append(actual_doc)
-        elif server_type == "listener" and actual_doc["value"]["rev"] != expected_docs[actual_doc["id"]]:
+        elif actual_doc["value"]["rev"] != expected_docs[actual_doc["id"]]:
             # Found the doc but unexpected rev on LiteServ
             missing_docs.append(actual_doc)
-        elif server_type == "sync_gateway" and actual_doc["_rev"] != expected_docs[actual_doc["_id"]]:
+
+    logging.debug("Missing Docs = {}".format(missing_docs))
+    log_info("Num found docs: {}".format(len(actual_docs["rows"]) - len(missing_docs)))
+    log_info("Num missing docs: {}".format(len(missing_docs)))
+
+    if len(missing_docs) > 0:
+        return False
+    elif expected_docs != actual_docs:
+        return False
+
+    return True
+
+
+def verify_docs_present_sync_gateway(expected_docs, actual_docs):
+    missing_docs = []
+
+    for actual_doc in actual_docs["rows"]:
+        if "error" in actual_doc or ("value" in actual_doc and len(actual_doc["value"]) == 0):
+            # Doc not found
+            missing_docs.append(actual_doc)
+        elif actual_doc["_rev"] != expected_docs[actual_doc["_id"]]:
             # Found the doc but unexpected rev on LiteServ
             missing_docs.append(actual_doc)
 
