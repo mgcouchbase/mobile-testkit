102af5bc9855adb5fff885edab225decbbda2315
Hemant <Hemant.rajput@couchbase.com>
2018-08-14 20:48:24 +0530

    Fixing exist API testcase

--- testsuites/CBLTester/CBL_Functional_tests/APITests/test_database.py
+++ testsuites/CBLTester/CBL_Functional_tests/APITests/test_database.py
@@ -213,9 +213,13 @@ class TestDatabase(object):
             pytest.skip("Test not supported on .net platforms or the db name is longer than or equal to 128 characters")
 
         db = self.db_obj.create(db_name)
-        path = self.db_obj.getPath(db)
+        path = self.db_obj.getPath(db).rstrip('/\\')
         log_info("path for db is {}".format(path))
-        path = os.path.dirname(path.rstrip('/\\'))
+        if '\\' in  path:
+            path = '\\'.join(path.split('\\')[:-1])
+        else:
+            path = '/'.join(path.split('/')[:-1])
+        print path
         assert self.db_obj.exists(db_name, path)
         assert self.db_obj.deleteDB(db) == -1
         assert not self.db_obj.exists(db_name, path)
