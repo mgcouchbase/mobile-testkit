b1ef22d1aff254282d725af2ac0003f43387fa83
Eunice Huang <eunice.huang@couchbase.com>
2019-06-07 11:07:34 -0700

    repro steps for cbse 6807

---
+++ testsuites/CBLTester/CBL_Functional_tests/TestSetup_FunctionalTests/test_cbse6807.py
@@ -0,0 +1,124 @@
+import pytest
+import time
+import os
+import random
+
+from keywords.MobileRestClient import MobileRestClient
+from keywords.ClusterKeywords import ClusterKeywords
+from keywords import couchbaseserver
+from keywords.utils import log_info
+from CBLClient.Database import Database
+from CBLClient.Replication import Replication
+from CBLClient.Document import Document
+from CBLClient.Authenticator import Authenticator
+from concurrent.futures import ThreadPoolExecutor
+
+from keywords.SyncGateway import sync_gateway_config_path_for_mode
+from keywords import document, attachment
+from libraries.testkit import cluster
+
+
+@pytest.fixture(scope="function")
+def setup_teardown_test(params_from_base_test_setup):
+    cbl_db_name = "cbl_db"
+    base_url = params_from_base_test_setup["base_url"]
+    db = Database(base_url)
+    db_config = db.configure()
+    log_info("Creating db")
+    cbl_db = db.create(cbl_db_name, db_config)
+
+    yield{
+        "db": db,
+        "cbl_db": cbl_db,
+        "cbl_db_name": cbl_db_name
+    }
+
+    log_info("Deleting the db")
+    db.deleteDB(cbl_db)
+
+
+@pytest.mark.sanity
+@pytest.mark.listener
+@pytest.mark.replication
+@pytest.mark.parametrize("num_of_docs, continuous", [
+    (3000, True),
+    (3000, False)
+])
+def test_frequent_replication(params_from_base_test_setup, num_of_docs, continuous):
+    """
+        @summary:
+        1. Create CBL DB and create bulk doc in CBL
+        2. Configure replication with valid values of valid cbl Db, valid target url
+        3. Start replication with push and pull
+        4. Verify replication is successful and verify docs exist
+    """
+    sg_db = "db"
+    sg_url = params_from_base_test_setup["sg_url"]
+    sg_admin_url = params_from_base_test_setup["sg_admin_url"]
+    cluster_config = params_from_base_test_setup["cluster_config"]
+    sg_blip_url = params_from_base_test_setup["target_url"]
+    base_url = params_from_base_test_setup["base_url"]
+    sg_config = params_from_base_test_setup["sg_config"]
+    db = params_from_base_test_setup["db"]
+    cbl_db = params_from_base_test_setup["source_db"]
+    sync_gateway_version = params_from_base_test_setup["sync_gateway_version"]
+
+    channels_sg = ["ABC"]
+    username = "autotest"
+    password = "password"
+    number_of_updates = 3000
+
+    # Create CBL database
+    sg_client = MobileRestClient()
+
+    # Reset cluster to ensure no data in system
+    c = cluster.Cluster(config=cluster_config)
+    c.reset(sg_config_path=sg_config)
+
+    db.create_bulk_docs(num_of_docs, "cbl", db=cbl_db, channels=channels_sg)
+
+    # Configure replication with push_pull
+    replicator = Replication(base_url)
+    sg_client.create_user(sg_admin_url, sg_db, username, password, channels=channels_sg)
+    session, replicator_authenticator, repl = replicator.create_session_configure_replicate(
+        base_url, sg_admin_url, sg_db, username, password, channels_sg, sg_client, cbl_db, sg_blip_url, continuous=continuous, replication_type="push_pull")
+
+    sg_docs = sg_client.get_all_docs(url=sg_url, db=sg_db, auth=session)
+    # sg_client.update_docs(url=sg_url, db=sg_db, docs=sg_docs["rows"], number_updates=number_of_updates, auth=session)
+    replicator.wait_until_replicator_idle(repl)
+    total = replicator.getTotal(repl)
+    completed = replicator.getCompleted(repl)
+    assert total == completed, "total is not equal to completed"
+    time.sleep(2)  # wait until replication is done
+    sg_docs = sg_client.get_all_docs(url=sg_admin_url, db=sg_db, include_docs=True)
+    sg_docs = sg_docs["rows"]
+
+    # Verify database doc counts
+    cbl_doc_count = db.getCount(cbl_db)
+    assert len(sg_docs) == cbl_doc_count, "Expected number of docs does not exist in sync-gateway after replication"
+
+    time.sleep(2)
+    cbl_doc_ids = db.getDocIds(cbl_db)
+    cbl_db_docs = db.getDocuments(cbl_db, cbl_doc_ids)
+    count = 0
+    for doc in cbl_doc_ids:
+        if continuous:
+            while count < 30:
+                time.sleep(0.5)
+                log_info("Checking {} for updates".format(doc))
+                if cbl_db_docs[doc]["updates"] == number_of_updates:
+                    break
+                else:
+                    log_info("{} is missing updates, Retrying...".format(doc))
+                    count += 1
+                    cbl_db_docs = db.getDocuments(cbl_db, cbl_doc_ids)
+            assert cbl_db_docs[doc]["updates"] == number_of_updates, "updates did not get updated"
+        else:
+            assert cbl_db_docs[doc]["updates"] == 0, "sync-gateway updates got pushed to CBL for one shot replication"
+    replicator.stop(repl)
+
+    total = replicator.getTotal(repl)
+    completed = replicator.getCompleted(repl)
+    replicator.stop(repl)
+    assert total == completed, "total is not equal to completed"
+
