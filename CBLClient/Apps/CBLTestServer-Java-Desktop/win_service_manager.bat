b4c5d0a1a4f0604e6bc5742a972063bcf36e5bbe
Eunice Huang <eunice.huang@couchbase.com>
2020-02-18 16:10:11 -0800

    add service user and start service for win service

--- CBLClient/Apps/CBLTestServer-Java-Desktop/win_service_manager.bat
+++ CBLClient/Apps/CBLTestServer-Java-Desktop/win_service_manager.bat
@@ -1,13 +1,16 @@
 echo off
-set location=%1
-set jar_name=%2
-set service_name=%3
-set status=%4
+set service_name=%1
+set status=%2
+set location=%3
+set jar_name=%4
+set service_user=%5
+set service_pwd=%6
 
 
 IF "%status%"=="start" (
-   %service_name%.exe //IS//%service_name% --Install=%location%\bin\%service_name%.exe --Description=%service_name% --Jvm=auto --Classpath=%location%\lib\%jar_name% --StartMode=jvm --StartClass=com.couchbase.mobiletestkit.javatestserver.TestServerMain --StartMethod=windowsService --StartParams=start --StopMode=jvm --StopClass=com.couchbase.mobiletestkit.javatestserver.TestServerMain --StopMethod=windowsService --StopParams=stop --LogPath=%location%\logs --StdOutput=auto --StdError=auto
-)
+  %service_name%.exe //IS//%service_name% --Install=%location%\\%service_name%.exe --Description=%service_name% --Jvm=auto --Classpath=%location%\\%jar_name% --StartMode=jvm --StartClass=com.couchbase.mobiletestkit.javatestserver.TestServerMain --StartMethod=windowsService --StartParams=start --StopMode=jvm --StopClass=com.couchbase.mobiletestkit.javatestserver.TestServerMain --StopMethod=windowsService --StopParams=stop --LogPath=%location%\\logs --StdOutput=auto --StdError=auto --ServiceUser=%service_user% --ServicePassword=%service_pwd%
+  
+  %service_name%.exe //ES//%service_name%
 ) ELSE (
   %service_name%.exe //DS//%service_name%
 )
